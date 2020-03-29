818abb078571306cd065a913845c0dd040fa09ba
maddybvh <maddybvh@gmail.com>
2020-03-29 11:11:09 -0400

    Adds constructor & renders books

--- script.js
+++ script.js
@@ -1,129 +1,53 @@
-//Determine if a string is a palindrome
-function palindrome(str) {
-    var re = '.';
-    var lowRegStr = str.toLowerCase().replace(re, '');
-    var reverseStr = lowRegStr.split('').reverse().join(''); 
-    return reverseStr === lowRegStr;
+let myLibrary = [];
+
+function Book(title, author, pages, readYet) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.readYet = readYet
+  this.info = function() {
+      return [this.title, this.author, this.pages, this.readYet]
   }
-   
-
-// Returns an array of palidromes between numLow and numHigh
-function palindromeArray(numLow, numHigh){
-    var palindromeArray = [];
-    let i = numLow;
-    do {
-        i+=.01;
-        let n = i.toFixed(2);
-        if (palindrome(n.toString())){
-            palindromeArray.push(parseFloat(n));         
-        }
-    } while (i <= numHigh);
-    return palindromeArray
 }
 
-var billAmount = document.getElementById("bill").value;
-var tipPercentLow = document.getElementById("tipPercentLow").value;
-var tipPercentHigh = document.getElementById("tipPercentHigh").value;
+const LittleMermaid = new Book('Little Mermaid', 'Author here', 890, 0)
+console.log(LittleMermaid.info());
 
-
-//Outputs all possible palindromic tips within the given parameters.
-function findPalTips (billAmount, tipPercentLow, tipPercentHigh){
-    tipArray = palindromeArray(billAmount*tipPercentLow*.01, billAmount*tipPercentHigh*.01);
-    return tipArray;
-};
-
-//
-function arrayTipsAndTotals(billAmount, tipArray){
-    let i;
-    var allTotalArray = [];
-    for (i of tipArray){
-        let total = Number(i)+ Number(billAmount)
-        let n = total.toFixed(2)
-        allTotalArray.push([i, n]);
-    }
-    return allTotalArray;
+function addBookToLibrary(newBook) {
+  myLibrary.push(newBook.info())
 }
 
-//Outputs an array of palindromic tips and their corresponding palendromic totals, if any exist.
-function findPalTotals (billAmount, tipArray){
-    let i;
-    var palTotalArray = [];
-    for (i of tipArray){
-        let total = Number(billAmount) + Number(i);
-        let n = total.toFixed(2)
-        if (palindrome(n.toString())){
-            palTotalArray.push([i, n]);
-        }
-    }
-    return palTotalArray
-}
-
-//Use DOM maniuplation to print the resulting bill, tip, and total combinations.
-function printResultsTable(billAmount, array){
-    var table = document.getElementById("resultsTable");
+function render(myLibrary, tableID){
+    var table = document.getElementById(tableID);
     //Build the table head
     var thead = table.createTHead();
     let headRow = thead.insertRow();
     var head0 = headRow.insertCell(0);
     var head1 = headRow.insertCell(1);
     var head2 = headRow.insertCell(2);
+    var head3 = headRow.insertCell(3);
 
-    head0.innerHTML = "<b>Bill</b>";
-    head1.innerHTML = "<b>Tip</b>";
-    head2.innerHTML = "<b>Total</b>";
-
+    head0.innerHTML = "<b>Title</b>";
+    head1.innerHTML = "<b>Author</b>";
+    head2.innerHTML = "<b>Pages</b>";
+    head3.innerHTML = "<b>Read</b>";
 
     //Print table results
     let i;
-    for (i = 0; i < array.length; i++){
+    for (i = 0; i < myLibrary.length; i++){
         var row = table.insertRow(i+1);
 
         let cell0 = row.insertCell(0);
         let cell1 = row.insertCell(1);
         let cell2 = row.insertCell(2);
+        let cell3 = row.insertCell(3);
 
-        cell0.innerHTML = "$" + billAmount;
-        cell1.innerHTML = "$" + array[i][0];
-        cell2.innerHTML = "$" + array[i][1];
+        cell0.innerHTML = myLibrary[i][0];
+        cell1.innerHTML = myLibrary[i][1];
+        cell2.innerHTML = myLibrary[i][2];
+        cell3.innerHTML = myLibrary[i][3];
     }
 }
 
-//Use DOM Manipulation to clear the table of old results
-function clearTable(){
-    var tableHeaderRowCount = 1;
-    var table = document.getElementById('resultsTable');
-    var rowCount = table.rows.length;
-    for (var i = tableHeaderRowCount; i <= rowCount; i++) {
-        table.deleteRow(-1);
-    }
-}
-
-
-
-//Event listener, main script calling all other scripts.
-document.getElementById("submitButton").addEventListener('click', (e) => {
-
-    clearTable();
-
-    billAmount = document.getElementById("bill").value;
-    tipPercentLow = document.getElementById("tipPercentLow").value;
-    tipPercentHigh = document.getElementById("tipPercentHigh").value;
-
-    tipArray = findPalTips (billAmount, tipPercentLow, tipPercentHigh)
-
-    palTotalArray = findPalTotals(billAmount, tipArray);
-
-    allTotalArray = arrayTipsAndTotals(billAmount, tipArray);
-
-    if (palTotalArray && palTotalArray.length){
-        document.getElementById("results").innerHTML = "Both tip and total can be palindromes!";
-        printResultsTable(billAmount, palTotalArray)
-    } else if (tipArray && tipArray.length){
-        document.getElementById("results").innerHTML = "You can tip in palindrome.";
-        printResultsTable(billAmount, allTotalArray)
-    } else {
-        document.getElementById("results").innerHTML = "Sorry, there are no palindromic tips available in that range.";
-    }
-}
-);
-
+addBookToLibrary(LittleMermaid)
+render(myLibrary, 'tableOfBooks');
