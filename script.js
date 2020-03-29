373ef9317f5812fb9b6e0f6b587de8a7e1e44279
maddybvh <maddybvh@gmail.com>
2020-03-29 17:17:26 -0400

    Updated to use this for rendering object properties

--- script.js
+++ script.js
@@ -1,52 +1,50 @@
 let myLibrary = [];
-let totalBooks = 0;
+
+const table = document.getElementById("tableOfBooks");
+
 
 function Book(title, author, pages, readYet) {
-    
-    this.uid = totalBooks
-    totalBooks += 1
 
     this.title = title
     this.author = author
     this.pages = pages
     this.readYet = readYet
     this.info = function() {
-        return [this.uid, this.title, this.author, this.pages, this.readYet]
+        return [this.title, this.author, this.pages, this.readYet]
   }
 }
 
-const LittleMermaid = new Book('Little Mermaid', 'Author here', 890, true)
-const HarryPotter1 = new Book('Harry Potter & The', 'JK Rowling', 309, false)
-const HarryPotter2 = new Book('Harry Potter 2', 'JK Rowling', 459, true)
-
 function addBookToLibrary(newBook) {
   myLibrary.push(newBook.info());
+  render(myLibrary);
 }
 
-function render(myLibrary, tableID){
-    var table = document.getElementById(tableID);
-    var row = table.insertRow(-1);
-
-    let cell0 = row.insertCell(0);
-    let cell1 = row.insertCell(1);
-    let cell2 = row.insertCell(2);
-    let cell3 = row.insertCell(3);
-    let cell4 = row.insertCell(4);
-
-    // render checkbox for Read boolean
-    let x = document.createElement("INPUT");
-    x.setAttribute("type", "checkbox");
-    x.checked = myLibrary[myLibrary.length - 1][4];
-    cell0.appendChild(x);
+function render(myLibrary) {
     
-    //Render the rest of the book info
-    cell1.innerHTML = myLibrary[myLibrary.length - 1][1];
-    cell2.innerHTML = myLibrary[myLibrary.length - 1][2];
-    cell3.innerHTML = myLibrary[myLibrary.length - 1][3];
+    for (let book of myLibrary) {
+        var row = table.insertRow(-1);
 
-    //Render delete button
-    cell4.innerHTML = "<button onclick='removeRow(this)'>x</button>";
-}
+        let cell0 = row.insertCell(0);
+        let cell1 = row.insertCell(1);
+        let cell2 = row.insertCell(2);
+        let cell3 = row.insertCell(3);
+        let cell4 = row.insertCell(4);
+
+        let x = document.createElement("INPUT");
+        x.setAttribute("type", "checkbox");
+        x.checked = book.readYet;
+        
+        cell0.appendChild(x);
+
+        cell1.innerHTML = this.title;
+        cell2.innerHTML = this.author;
+        cell3.innerHTML = this.pages;
+        cell4.innerHTML = "<button class='remove'>x</button>";
+
+        addRemoveListeners();
+        addReadListeners();
+    }    
+};
 
 //Event listener for add new book
 document.getElementById("addNewBookButton").addEventListener('click', (e) => {
@@ -58,15 +56,33 @@ document.getElementById("addNewBookButton").addEventListener('click', (e) => {
     
     let newBook = new Book(title, author, pages, readYet);
     addBookToLibrary(newBook);
-
-    render(myLibrary, 'tableOfBooks');
-
 }
 );
 
-//Event listener for deleting book rows from the rendered table
-removeRow = function(cellToRemove) {
-    p = cellToRemove.parentNode.parentNode;
-    console.log(p);
-    p.parentNode.removeChild(p);          
+
+function addRemoveListeners() {
+    document.querySelectorAll(".remove").forEach((button) => {
+        button.addEventListener("click", () => {
+            removeBook(button.parentElement.parentElement);
+        })
+    })
+}
+
+function removeBook(selectedRow) {
+    const bookTitle = selectedRow.children[0].innerHTML; // gets title of book on row
+    table.deleteRow(selectedRow.rowIndex);               // removes row from table
+
+    for (i = 0; i < myLibrary.length; i++) {             // finds book title in myLibrary
+        if (myLibrary[i].title == bookTitle) {           // and splices out book
+            myLibrary.splice(i, 1); 
+        }
+    }
+}
+
+function addReadListeners() {
+    document.querySelectorAll(".checkbox").forEach((checkbox) => {
+        select.addEventListener("change", () => {
+            updatedReadStatus(selectedRow);
+        })
+    })
 }
